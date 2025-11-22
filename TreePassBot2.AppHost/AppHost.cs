aefb498c64b6a8240f77efe82c05aef7c6b125d8
WhiteCAT <64885812+whitecat346@users.noreply.github.com>
2025-11-22 21:02:03 +0800

    feat(web): add base web view

--- TreePassBot2.AppHost/AppHost.cs
+++ TreePassBot2.AppHost/AppHost.cs
@@ -2,4 +2,6 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddProject<Projects.TreePassBot2>("treepassbot2");
 
+builder.AddProject<Projects.BackManager_Server>("backmanager-server");
+
 builder.Build().Run();
